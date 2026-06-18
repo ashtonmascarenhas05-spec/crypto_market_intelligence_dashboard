460755ff5a303fd8b4a3bbcc9db0e3c89fd62360
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-18 18:28:20 +0530

    Developing Scraper Engine

--- core/scraper.py
+++ core/scraper.py
@@ -1,1 +1,11 @@
-from concurrent.futures import ThreadPoolExecutor
+import threading
+import time
+
+class ScraperEngine:
+    def __init__(self,base_urls,headers,timeout,rate_limit):
+        self.base_urls = base_urls
+        self.headers = headers
+        self.timeout = timeout
+        self.rate_limit = rate_limit
+        self.results = []
+        self.lock = threading.Lock()
