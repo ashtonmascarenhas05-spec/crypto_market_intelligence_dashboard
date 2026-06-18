cce93bf5158df217587c498a79bab1962235991d
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-18 19:37:11 +0530

    Developed the required functions of the ScraperEngine Class

--- core/scraper.py
+++ core/scraper.py
@@ -1,7 +1,7 @@
 import threading
 import time
 import requests
-from threading import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 class ScraperEngine:
     def __init__(self,base_urls,headers,timeout,rate_limit,expected_keys):
@@ -19,22 +19,26 @@ class ScraperEngine:
             response = requests.get(url, headers=self.headers, timeout=self.timeout)
             if response.status_code == 200:
                 data = response.json()
-                if self.validate_data(data, self.expected_keys):
+                if self.validate_data(data):
                     with self.lock:
                         self.results.append(data)
-            time.sleep(self.rate_limit)  # Simulate rate limiting
-            return f"Fetched data from {url}"
+                time.sleep(self.rate_limit)  # Simulate rate limiting
+                return f"Fetched data from {url}"
+            else:
+                return f"Failed to fetch data from {url}: Status code {response.status_code}"
         except Exception as e:
             return f"Error fetching data from {url}: {str(e)}"
         
     def fetch_all(self):
         with ThreadPoolExecutor(max_workers= 3) as executor:
-            results = executor.map(self.fetch_single, self.base_urls)
-        return list(results)
-    
+            status_messages = list(executor.map(self.fetch_single, self.base_urls))
+            for message in status_messages:
+                print(message)
+        return list(self.results)
+
     ##checks if the returned data has the expected keys.
-    def validate_data(self, data, expected_keys):  # Example expected keys
-        for key in expected_keys:
+    def validate_data(self, data):  # Example expected keys
+        for key in self.expected_keys:
             if key not in data:
                 return False
         return True    
