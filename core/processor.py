6566dcdea6a07165ae12c3082be6fb73ebe24e48
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-20 21:36:45 +0530

    Checking of null values

--- core/processor.py
+++ core/processor.py
@@ -3,3 +3,4 @@ import pandas as pd
 class DataProcessor:
     def __init__(self,raw_data):
         df = pd.DataFrame(raw_data)       ## Converting the raw data into panda dataframe
+        print(df.isnull().sum()) ## checking null values in the raw_data
