53179b31cf57e49dcbf7ba4217ac4319cd2e1850
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-21 20:03:48 +0530

    Added a new method named clean_data that will fill the null values with 0

--- core/processor.py
+++ core/processor.py
@@ -5,4 +5,8 @@ class DataProcessor:
     def __init__(self,raw_data):
         self.raw_data = raw_data  # class receives a list of dictionaries from the scraper
         self.df = None   # we will store our clean dataframe here
-        
+    def clean_data(self):
+        df = pd.DataFrame(self.raw_data)     #It shall read the raw_data into a Pandas DataFrame
+        df.fillna(0)          ## It fill the missing values with value 0
+        df['TimeStamp'] = pd.Timestamp.now()    # Adding a TimeStamp
+        self.df = df
