cd49f6fb7772d97e40a6ca72bb8d186f6d49b7ad
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-21 19:43:37 +0530

    Created Data Processor Class

--- core/processor.py
+++ core/processor.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd 
 
 class DataProcessor:
     def __init__(self,raw_data):
-        df = pd.DataFrame(raw_data)       ## Converting the raw data into panda dataframe
-        print(df.isnull().sum()) ## checking null values in the raw_data
+        self.raw_data = raw_data  # class receives a list of dictionaries from the scraper
+        self.df = None   # we will store our clean dataframe here
+        
