769bf4c87dd2172fcb390cec2499627b0402251b
Ashton Mascarenhas <ashtonmascarenhas05@gmail.com>
2026-06-23 22:32:40 +0530

    Added a method that creates an SQL table in the database

--- core/database.py
+++ core/database.py
@@ -6,5 +6,21 @@ class DatabaseManager:
         self.create_table()
 
     def create_table(self):
-        #Queries to create a  table has to be written here
-        pass
+        #Establish a connection and a cursor
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+
+        schema_query ="""
+        CREATE TABLE IF NOT EXISTS crypto_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        coin TEXT,
+        price REAL,
+        timestamp TEXT,
+        log_price REAL,
+        normalized_price REAL
+        )
+        """
+
+        cursor.execute(schema_query)
+        conn.commit()
+        conn.close()
